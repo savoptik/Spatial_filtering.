7c1e5d5e73e2d44eb45af832b1e4601d229cca5e
savoptik <sav70792@gmail.com>
2018-05-04 14:21:53 +0300

    Initial Commit

---
+++ Spatial_filtering./main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Spatial_filtering.
+//
+//  Created by Артём Семёнов on 04.05.2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
