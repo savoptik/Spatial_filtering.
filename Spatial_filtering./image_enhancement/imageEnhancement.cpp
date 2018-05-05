15de17364d4798096e621b93bbc3f787ccfa16da
savoptik <sav70792@gmail.com>
2018-05-05 16:10:30 +0300

    Описана структура класса.

---
+++ Spatial_filtering./image_enhancement/imageEnhancement.cpp
@@ -0,0 +1,9 @@
+//
+//  imageEnhancement.cpp
+//  Spatial_filtering.
+//
+//  Created by Артём Семёнов on 05.05.2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include "imageEnhancement.hpp"
---
+++ Spatial_filtering./image_enhancement/imageEnhancement.hpp
@@ -0,0 +1,25 @@
+//
+//  imageEnhancement.hpp
+//  Spatial_filtering.
+//
+//  Created by Артём Семёнов on 05.05.2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#ifndef imageEnhancement_hpp
+#define imageEnhancement_hpp
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+class imageEnhancement {
+public:
+    cv::Mat laplasianing(cv::Mat& matrix);
+    cv::Mat gradienting(cv::Mat& matrix);
+    cv::Mat smoothing(cv::Mat& matrix);
+    cv::Mat& extractVorkMatrix();
+private:
+    cv::Mat vorkMatrix;
+};
+
+#endif /* imageEnhancement_hpp */
