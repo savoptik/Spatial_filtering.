6849ea84b218aeccd636beed217ed5d18c408445
savoptik <sav70792@gmail.com>
2018-05-05 19:44:11 +0300

    Пересмотренна концепция.

--- Spatial_filtering./main.cpp
+++ Spatial_filtering./main.cpp
@@ -7,17 +7,34 @@
 //
 
 #include <iostream>
-#include "matrixContainer.hpp"
-#include "imageEnhancement.hpp"
-using namespace std;
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+using namespace cv;
 
-const string filePath = "skeleton2.png";
+const std::string filePath = "skeleton2.png";
 
 int main(int argc, const char * argv[]) {
-    matrixContainer container(filePath); // создание контейнера.
-    container.showImage(); // вывод изображения для контроля.
-    imageEnhancement laplacian(container.extractImage());
-    laplacian.laplasianing();
-    laplacian.showCurrentState();
+    auto img = imread(filePath); // чтени файла.
+    imshow("first", img);
+    waitKey();
+    Mat laplacian;
+    Laplacian(img, laplacian, 3); // лапласиан.
+    convertScaleAbs(laplacian, laplacian);
+    auto laplacianAddIMG = laplacian + img; // наложение лапласиана на изображение.
+    namedWindow("laplacian", CV_WINDOW_AUTOSIZE);
+    Mat grad;
+    Sobel(img, grad, 3, 1, 1);
+    Mat smooth;
+    medianBlur(grad, smooth, 3);
+    Mat multiplet = (laplacianAddIMG/255.0) * (smooth/255.0);
+    Mat multipleAddIMG = img/255 + multiplet;
+    Mat result = multipleAddIMG;
+    imshow("result", result);
+    waitKey();
+    destroyAllWindows();
     return 0;
 }
