ebf013bfc93a45ec62a217429e35b281f57ec21f
savoptik <sav70792@gmail.com>
2018-05-05 16:14:41 +0300

    подготовленны описания методов.

--- Spatial_filtering./image_enhancement/imageEnhancement.cpp
+++ Spatial_filtering./image_enhancement/imageEnhancement.cpp
@@ -7,3 +7,24 @@
 //
 
 #include "imageEnhancement.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+using namespace cv;
+
+cv::Mat imageEnhancement::laplasianing(cv::Mat &matrix) { 
+    <#code#>;
+}
+
+cv::Mat imageEnhancement::gradienting(cv::Mat &matrix) { 
+    <#code#>;
+}
+
+cv::Mat imageEnhancement::smoothing(cv::Mat &matrix) { 
+    <#code#>;
+}
+
+cv::Mat &imageEnhancement::extractVorkMatrix() { 
+    <#code#>;
+}
